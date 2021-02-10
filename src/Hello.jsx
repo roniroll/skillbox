fc0e1b26c8f51edd892e2daf258a16275330b1ec
Nikita Nedospasov <nedospasiik@gmail.com>
2021-02-10 10:30:41 +0300

    Ошибка с less

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,9 +1,10 @@
-import * as React from 'react'
+import * as React from 'react';
+import styles from './Hellow.less';
 
 export function Hello () {
     return (
-        <h1>
-            Hellow react!!
+        <h1 className={styles.exm}>
+            Hello React
         </h1>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,24 @@ module.exports = {
         rules: [{
             test: /\.[tj]sx?$/,
             use: ['ts-loader']
-        }]
+        }, 
+        {
+            test: /\.less$/,
+            use: 
+                ['style-loader', 
+                {
+                    loader: 'css-loader',
+                    options: {
+                        modules: {
+                            mode: 'local',
+                            localIdentName: '[name]__[local]--[hash:base64:5]'
+                                 }
+                             }
+                },
+                   'less-loader'
+                ]
+        }   
+    ]
     },
     devServer: {
         port: 3000,
